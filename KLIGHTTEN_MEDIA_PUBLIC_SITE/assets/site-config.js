fba727cde41250733522617a92b9750607524acc
Jardeleza0921 <jjardeleza@paterostechnologicalcollege.edu.ph>
2026-08-23 23:53:44 +0800

    Delete KLIGHTTEN_MEDIA_PUBLIC_SITE directory

--- KLIGHTTEN_MEDIA_PUBLIC_SITE/assets/site-config.js
+++
@@ -1,7 +0,0 @@
-window.KLIGHTTEN_SITE = Object.freeze({
-  version: "2.7.0",
-  releaseFile: "KLIGHTTEN_MEDIA_2.7_POLISHED_CONTROLS.zip",
-  githubRepository: "",
-  supportEmail: "",
-  customDomain: ""
-});
